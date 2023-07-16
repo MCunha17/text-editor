e49444b165a51a19b676ed833f18878819313ed0
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-15 20:59:30 -0500

    Configured workbox plugs and added CSS loaders and babel to webpack.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,27 +3,65 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Add and configure workbox plugins for a service worker and manifest file.
+// Add CSS loaders and babel to webpack.
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        chunks: ['install'],
+        filename: 'install.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'Your Text Editor',
+        short_name: 'Text Editor',
+        description: 'A Progressive Web App text editor',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/service-worker.js',
+        exclude: [/\.map$/, /asset-manifest\.json$/],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
