dd127e174924598d042410483385fc7b18bdf4b8
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-15 21:02:15 -0500

    Added database logic.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    const db = await openDB('jate', 1);
+    const transaction = db.transaction('jate', 'readwrite');
+    const store = transaction.objectStore('jate');
+    await store.add({ content });
+    console.log('Content added to database:', content);
+  } catch (error) {
+    console.error('Error adding content to database:', error);
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const transaction = db.transaction('jate', 'readonly');
+    const store = transaction.objectStore('jate');
+    const allContent = await store.getAll();
+    console.log('All content retrieved from database:', allContent);
+    return allContent;
+  } catch (error) {
+    console.error('Error retrieving content from database:', error);
+    return [];
+  }
+};
 
 initdb();
