24e06e48a40858b10775837b45e75e13bfeb8c3d
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-07-20 17:27:31 -0500

    Updated code's store.add instead of store.put

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,36 +7,36 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate');
       console.log('jate database created');
     },
   });
 
-// Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and puts it in the database
 export const putDb = async (content) => {
   try {
     const db = await openDB('jate', 1);
     const transaction = db.transaction('jate', 'readwrite');
     const store = transaction.objectStore('jate');
-    await store.add({ content });
+    await store.put(content, 'content');
     console.log('Content added to database:', content);
   } catch (error) {
     console.error('Error adding content to database:', error);
   }
 };
 
-// Add logic for a method that gets all the content from the database
+// Add logic for a method that gets the content from the database
 export const getDb = async () => {
   try {
     const db = await openDB('jate', 1);
     const transaction = db.transaction('jate', 'readonly');
     const store = transaction.objectStore('jate');
-    const allContent = await store.getAll();
-    console.log('All content retrieved from database:', allContent);
-    return allContent;
+    const content = await store.get('content');
+    console.log('Content retrieved from database:', content);
+    return content;
   } catch (error) {
     console.error('Error retrieving content from database:', error);
-    return [];
+    return null;
   }
 };
 
